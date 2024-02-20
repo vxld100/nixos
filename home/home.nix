30c8da3c1ac49d1153252272bc780801ab617788
Lilin <vxld100@tuta.io>
2024-02-20 22:15:40 +0100

    Added zsh config to home manager. I re-encrypted the secrets accidentally.

--- home/home.nix
+++ home/home.nix
@@ -1,88 +1,136 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "lilin";
-  home.homeDirectory = "/home/lilin";
+	# Home Manager needs a bit of information about you and the paths it should
+	# manage.
+	home.username = "lilin";
+	home.homeDirectory = "/home/lilin";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+	# No idea how to get this to work. Why the fuck is EVERYTHING in nix different?! Can't even declare variables.
+	#systemConfigDirectory = "${home.homeDirectory}/Nixos/system/configuration.nix";
+	#homeConfigDirectory = "${home.homeDirectory}/Nixos/home/home.nix";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+	# This value determines the Home Manager release that your configuration is
+	# compatible with. This helps avoid breakage when a new Home Manager release
+	# introduces backwards incompatible changes.
+	#
+	# You should not change this value, even if you update Home Manager. If you do
+	# want to update the value, then make sure to first check the Home Manager
+	# release notes.
+	home.stateVersion = "23.11"; # Please read the comment before changing.
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+	# The home.packages option allows you to install Nix packages into your
+	# environment.
+	home.packages = with pkgs; [
+		# # Adds the 'hello' command to your environment. It prints a friendly
+		# # "Hello, world!" when run.
+		# pkgs.hello
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-	   cliphist
-		wl-clipboard
-	   neofetch
-		micromamba
-		zathura
-		anki
-		lesspass-cli
-	 ];
+	 	 # It is sometimes useful to fine-tune packages, for example, by applying
+	 	 # overrides. You can do that directly here, just don't forget the
+	 	 # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+	 	 # fonts?
+	 	 (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+	 	 # You can also create simple shell scripts directly inside your
+	 	 # configuration. For example, this adds a command 'my-hello' to your
+	 	 # environment:
+	 	# (pkgs.writeShellScriptBin "my-hello" ''
+	 	#   echo "Hello, ${config.home.username}!"
+	 	# '')
+			cliphist
+			wl-clipboard
+			neofetch
+			micromamba
+			zathura
+			anki
+			lesspass-cli
+	 	];
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+		# Home Manager is pretty good at managing dotfiles. The primary way to manage
+		# plain files is through 'home.file'.
+	home.file = {
+		# # Building this configuration will create a copy of 'dotfiles/screenrc' in
+	 	# # the Nix store. Activating the configuration will then make '~/.screenrc' a
+	 	# # symlink to the Nix store copy.
+	 	# ".screenrc".source = dotfiles/screenrc;
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/lilin/etc/profile.d/hm-session-vars.sh
-  #
-  # Note that this works only when home manager is managing the shell. Otherwise any such variables have to be set somewhere else
-  home.sessionVariables = {
-    #EDITOR = "nvim";
-	 #SUDO_EDITOR = "nvim";
-	 WLR_NO_HARDWARE_CURSORS=1; # This way the cursor is not invisible on wayland
-  };
+	 	# # You can also set the file content immediately.
+	 	# ".gradle/gradle.properties".text = ''
+	 	#   org.gradle.console=verbose
+	 	#   org.gradle.daemon.idletimeout=3600000
+	 	# '';
+	};
+
+	# Home Manager can also manage your environment variables through
+	# 'home.sessionVariables'. If you don't want to manage your shell through Home
+	# Manager then you have to manually source 'hm-session-vars.sh' located at
+	# either
+	#
+	#  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+	#
+	# or
+	#
+	#  /etc/profiles/per-user/lilin/etc/profile.d/hm-session-vars.sh
+	#
+	# Note that this works only when home manager is managing the shell. Otherwise any such variables have to be set somewhere else
+	home.sessionVariables = {
+	 	#EDITOR = "nvim";
+	 	#SUDO_EDITOR = "nvim";
+	 	WLR_NO_HARDWARE_CURSORS=1; # This way the cursor is not invisible on wayland
+	};
+
+	programs.zsh = {
+		enable = true;
+		autocd = true;
+
+		shellAliases = {
+			ll = "ls -Alhp";
+			la = "ls -Ahp";
+			rg = "ranger";
+			vim = "nvim $1";
+			update = "sudo nixos-rebuild switch -I nixos-config=$HOME/NixOS/system/configuration.nix";
+			home = "home-manager switch -f $HOME/NixOS/home/home.nix";
+		};
+
+		syntaxHighlighting = {
+			enable = true;
+			styles = {
+				command = "none";
+				alias = "none";
+				builtin = "none";
+				precommand = "fg=magenta,bold";
+				function = "fg=magenta";
+				unknown-token = "fg=red";
+			};
+		};
+
+		antidote = {
+			enable = true;
+			plugins = [
+				"romkatv/powerlevel10k"
+			];
+		};
+
+		initExtraFirst = ''
+if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+	source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+fi
+		'';
+		initExtra = ''
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+		'';
+
+	};
 
 
-  programs.git = {
-    enable = true;
-    userName  = "Lilin";
-    userEmail = "vxld100@tuta.io";
-  };
+	programs.git = {
+	 	enable = true;
+	 	userName  = "Lilin";
+	 	userEmail = "vxld100@tuta.io";
+	};
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+	# Let Home Manager install and manage itself.
+	programs.home-manager.enable = true;
 }
