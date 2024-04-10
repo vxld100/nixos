bd40899662b464f399ebb2b00e085600e003c610
Lilin <vxld100@tuta.io>
2024-04-10 12:20:28 +0200

    Added vivaldi.

--- system/configuration.nix
+++ system/configuration.nix
@@ -166,7 +166,9 @@
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+	 curl
     firefox
+	 vivaldi
     alacritty
     ranger
     dolphin
