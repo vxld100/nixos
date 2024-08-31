0b65329634c210c8300f31d62a45afb5ba0daa00
Lilin <vxld100@tuta.io>
2024-09-01 00:14:16 +0200

    Just some minor refactoring.

--- system/configuration.nix
+++ system/configuration.nix
@@ -213,10 +213,7 @@
   };
 
   services = {
-      jellyfin = {
-        enable = true;
-        group = "multimedia";
-      };
+      jellyfin = { enable = true; group = "multimedia"; };
       sonarr = { enable = true; group = "multimedia"; };
       radarr = { enable = true; group = "multimedia"; };
 		prowlarr = { enable = true; };
@@ -231,29 +228,6 @@
 			};
 		};
 	};
-	
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
