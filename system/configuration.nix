e8a9f7d079bfc3786f89bcb13e22ff3f0976ab08
Lilin <vxld100@tuta.io>
2025-02-12 13:56:54 +0100

    Renamed it back in order to avoid any bad surprises.

--- system/configuration.nix
+++ system/configuration.nix
@@ -101,16 +101,7 @@
 #};
 
   fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-      noto-fonts-emoji
-      liberation_ttf
-      fira-code
-      fira-code-symbols
-      mplus-outline-fonts.githubRelease
-      dina-font
-      proggyfonts
-      nerdfonts
+    nerdfonts
   ];
 
 # Enable Hyprland and the X11 windowing system.
