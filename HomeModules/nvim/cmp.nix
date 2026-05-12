cbf0b6ba13b9960f66b1ec6577f4e31d9a278d85
Lilin <vxld100@tuta.io>
2026-05-12 15:55:09 +0200

    Made it so I can use my snippets more easily and have latex symbols :)

--- HomeModules/nvim/cmp.nix
+++ HomeModules/nvim/cmp.nix
@@ -4,12 +4,18 @@
 
   plugins.cmp = {
     enable = true;
+    enabled = ''
+      function()
+        return not require("luasnip").in_snippet()
+      end
+    '';
     autoEnableSources = true;
     settings = {
       sources = [
         { name = "nvim_lsp"; }
         { name = "luasnip"; }
         { name = "path"; }
+        { name = "latex_symbols"; }
         { name = "buffer"; }
       ];
       mapping = {
@@ -21,10 +27,10 @@
               local line, col = unpack(vim.api.nvim_win_get_cursor(0))
               return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
             end
-            if cmp.visible() then
+            if luasnip.in_snippet() and luasnip.jumpable(1) then
+              luasnip.jump(1)
+            elseif cmp.visible() then
               cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
-            -- they way you will only jump inside the snippet region
             elseif luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
             elseif has_words_before() then
