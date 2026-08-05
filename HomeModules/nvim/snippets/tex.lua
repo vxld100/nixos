31970b7ab33c49ea594e2c8246dc5f10db2af12a
Lilin <vxld100@tuta.io>
2026-08-05 12:25:19 +0200

    Much progress with the snippets.

--- HomeModules/nvim/snippets/tex.lua
+++ HomeModules/nvim/snippets/tex.lua
@@ -2,21 +2,253 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local rep = require("luasnip.extras").rep
 local fmta = require("luasnip.extras.fmt").fmta
+local line_begin = require("luasnip.extras.conditions.expand").line_begin
 
--- Condition: only expand in math mode (requires VimTeX)
+-- 1. Condition: only expand in math mode (requires VimTeX)
 local in_mathzone = function()
   return vim.fn['vimtex#syntax#in_mathzone']() == 1
 end
 
-return {
-   s(
-      {
-	 trig = " ff ",
-	 snippetType = "autosnippet",
-	 condition = in_mathzone,
-      },
-      { fmta("\\frac{<>}{<>}", { i(1), i(2) }) }
-   ),
+-- 2. Helper for simple math text replacements
+local function math_tex(trig, expansion)
+  return s(
+    { trig = trig, snippetType = "autosnippet", wordTrig = true, condition = in_mathzone },
+    t(expansion)
+  )
+end
+
+-- 3. Greek letters and math symbols
+local symbols = {
+  alpha  = "\\alpha",
+  beta   = "\\beta",
+  gamma  = "\\gamma",
+  theta  = "\\theta",
+  mu     = "\\mu",
+  phi    = "\\phi",
+  psi    = "\\psi",
+  rho    = "\\rho",
+  ooo    = "\\infty",
+  xx     = "\\times",
+  ["in"] = "\\in",
+  fa     = "\\forall",
+  cd     = "\\cdot",
+  ra     = "\\righarrow",
+  la     = "\\leftarrow",
+  iff    = "\\iff",
+  dots   = "\\dots",
+  ldots  = "\\ldots",
+  ["=>"] = "\\implies",
+  ["!="] = "\\neq",
+  ["<="] = "\\leq",
+  [">="] = "\\geq",
 }
 
+local math_symbols = {}
+for trig, sym in pairs(symbols) do
+   trig = trig.." " --concatenation
+   sym = sym.." " -- concatentaion so it doens't replace the space beforehand (that's ugly)
+  table.insert(math_symbols, math_tex(trig, sym))
+end
+
+-- Environments & complex snippets
+local environments = {
+  s( {
+      trig = "beg ",
+      snippetType = "autosnippet",
+      wordTrig = false,
+      condition = line_begin,
+    },
+    fmta(
+      [[
+      \begin{<>}
+      <> 
+      \end{<>}
+      ]],
+      { i(1), i(2), rep(1) })),
+  s( {
+      trig = "dm ",
+      snippetType = "autosnippet",
+      wordTrig = false,
+      condition = line_begin,
+    },
+    fmta(
+      [[
+      \[
+      <>
+      \]
+      ]],
+      { i(1) })),
+  s( {
+      trig = "eq ",
+      snippetType = "autosnippet",
+      wordTrig = false,
+      condition = line_begin,
+    },
+    fmta(
+      [[
+      \begin{equation}
+      <>
+      \end{equation}
+      ]],
+      { i(1) })),
+  s( {
+      trig = "eqa ",
+      snippetType = "autosnippet",
+      wordTrig = false,
+      condition = line_begin,
+    },
+    fmta(
+      [[
+      \begin{equation*}
+      <>
+      \end{equation*}
+      ]],
+      { i(1) })),
+  s( {
+      trig = "ali ",
+      snippetType = "autosnippet",
+      wordTrig = false,
+      condition = line_begin,
+    },
+    fmta(
+      [[
+      \begin{equation}
+      \begin{aligned}
+      <>
+      \end{aligned}
+      \end{equation}
+      ]],
+      { i(1) })),
+  s( {
+      trig = "ali ",
+      snippetType = "autosnippet",
+      wordTrig = false,
+      condition = line_begin,
+    },
+    fmta(
+      [[
+      \begin{equation}
+      \begin{aligned}
+      <>
+      \end{aligned}
+      \end{equation}
+      ]],
+      { i(1) })),
+  s( {
+      trig = "enum ",
+      snippetType = "autosnippet",
+      wordTrig = false,
+      condition = line_begin,
+    },
+    fmta(
+      [[
+      \begin{enumeration}
+      \item <>
+      \end{enumeration}
+      ]],
+      { i(1) })),
+}
+
+local formatting = {
+  s( {
+      trig = "^",
+      snippetType = "autosnippet",
+      wordTrig = false,
+      condition = in_mathzone,
+    },
+    fmta("^{ <> } <>", { i(1), i(2) })),
+  s( {
+      trig = "_",
+      snippetType = "autosnippet",
+      wordTrig = false,
+      condition = in_mathzone,
+    },
+    fmta("_{ <> } <>", { i(1), i(2) })),
+}
+
+local operators = {
+  s( {
+      trig = "ff ",
+      snippetType = "autosnippet",
+      condition = in_mathzone,
+    },
+    fmta("\\frac{ <> }{ <> } <>", { i(1), i(2), i(3) })),
+  s( {
+      trig = "/ ",
+      snippetType = "autosnippet",
+      condition = in_mathzone,
+    },
+    fmta("\\frac{ ${VISUAL} }{ <> } <>", { i(1), i(2) })),
+  s( {
+      trig = "lim ",
+      snippetType = "autosnippet",
+      condition = in_mathzone,
+    },
+    fmta("\\lim_{ <> \\to <> } <> ", { i(1, "n"), i(2, "\\infty"), i(3) })),
+  s( {
+      trig = "sum ",
+      snippetType = "autosnippet",
+      condition = in_mathzone,
+    },
+    fmta("\\sum_{ <> }^{ <> } <>", { i(1, "n=0"), i(2, "\\infty"), i(3) })),
+  s( {
+      trig = "int ",
+      snippetType = "autosnippet",
+      condition = in_mathzone,
+    },
+    fmta("\\int_{ <> }^{ <> } <> d{ <> } <>", { i(1, "-\\infty"), i(2, "\\infty"), i(3), i(4, "x"), i(5) })),
+}
+
+local functions = {
+  s(
+    {
+      trig = "sin ",
+      snippetType = "autosnippet",
+      condition = in_mathzone,
+    },
+    fmta("\\sin\\left( <> \\right) <>", { i(1), i(2) })
+  ),
+  s(
+    {
+      trig = "cos ",
+      snippetType = "autosnippet",
+      condition = in_mathzone,
+    },
+    fmta("\\cos\\left( <> \\right) <>", { i(1), i(2) })
+  ),
+  s(
+    {
+      trig = "tan ",
+      snippetType = "autosnippet",
+      condition = in_mathzone,
+    },
+    fmta("\\tan\\left( <> \\right) <>", { i(1), i(2) })
+  ),
+  s(
+    {
+      trig = "e ",
+      snippetType = "autosnippet",
+      condition = in_mathzone,
+    },
+    fmta("\\e^{ <> } <>", { i(1), i(2) })
+  ),
+}
+
+-- 5. Combine and Register
+local all_snippets = {}
+
+local function append_snippets(source_table)
+  for _, snippet in ipairs(source_table) do
+    table.insert(all_snippets, snippet)
+  end
+end
+
+append_snippets(math_symbols)
+append_snippets(environments)
+append_snippets(formatting)
+append_snippets(operators)
+append_snippets(functions)
+
+ls.add_snippets("tex", all_snippets)
